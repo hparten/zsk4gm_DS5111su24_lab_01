d4785970070826ecb3553888486b481ac4c7cf34
Ubuntu <ubuntu@ip-172-31-24-65.ec2.internal>
2024-06-16 19:35:31 +0000

    week 2 lab

--- makefile
+++ makefile
@@ -25,3 +25,8 @@ total_lines:
 
 total_words:
 	@cat *.txt | wc -w
+
+setup: 
+	python3 -m venv env
+	./env/bin/pip install --upgrade pip 
+	./env/bin/pip install -r requirements.txt
---
+++ process_text.py
@@ -0,0 +1,54 @@
+import string
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+
+
+def clean_text(text):
+	assert isinstance(text, str)
+	logging.debug(f'Cleaning text: {text}'), 'Input must be a string' 
+
+	text = text.lower()
+	cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+	
+	assert isinstance(cleaned_text, str), 'Output must be a string'
+	assert cleaned_text, 'Check output text is not None'
+	logging.debug(f'Cleaned text: {cleaned_text}')
+	
+	return cleaned_text
+
+def tokenize(text):
+	assert isinstance(text, str), 'Input must be a string'
+	logging.debug(f'Tokenizing text: {text}')
+	
+	words = text.split()
+	
+	assert isinstance(words, list), 'Output must be a list'	
+	assert all(isinstance(word, str) for word in words), 'All words in list must be strings' 
+	logging.debug(f'Tokenized words: {words}')
+	
+	return words
+
+def count_words(text):
+    assert isinstance(text, str), 'Input must be a string'
+    logging.debug(f'Counting words in text {text}')
+
+    cleaned_text = clean_text(text)
+    tokens = tokenize(cleaned_text)
+
+    word_count = {}
+    for word in tokens:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    assert isinstance(word_count, dict), 'Output must be a dict'
+    assert all(isinstance(word, str) for word in word_count.keys()), 'All keys must be str'
+    assert all(isinstance(count, int) for count in word_count.values()), 'All counts must be int'
+    logging.debug(f'Counts for each word: {word_count}')
+
+    return word_count
+
+
