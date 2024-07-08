b6f8ada09f5a231bf5c526c13eecf74ba99a7002
Ubuntu <ubuntu@ip-172-31-24-76.ec2.internal>
2024-07-08 01:39:45 +0000

    corrected logging issue in init file

--- src/zsk4gm/__init__.py
+++ src/zsk4gm/__init__.py
@@ -1,3 +1,15 @@
 from zsk4gm.process_text import clean_text
 from zsk4gm.process_text import tokenize
 from zsk4gm.process_text import count_words
+
+import os
+import logging
+
+log_file_path = os.path.join(os.path.dirname(__file__), 'logs', 'logfile.log')
+log_dir = os.path.dirname(log_file_path)
+
+# Ensure directory exists
+os.makedirs(log_dir, exist_ok=True)
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s', filename=log_file_path)
